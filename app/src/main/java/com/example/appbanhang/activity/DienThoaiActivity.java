a90b90a7e14014037d00136adc64ef7e881b9304
HieuHoangit <97137357+HieuHoangit@users.noreply.github.com>
2022-05-19 21:34:17 +0700

    CuoiKi
    AppBanDienThoai

---
+++ app/src/main/java/com/example/appbanhang/activity/DienThoaiActivity.java
@@ -0,0 +1,46 @@
+package com.example.appbanhang.activity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.example.appbanhang.R;
+
+public class DienThoaiActivity extends AppCompatActivity {
+    //Toolbar toolbar;
+    //RecyclerView recyclerView;
+
+
+
+
+   // @Override
+    //protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_dien_thoai);
+
+       // AnhXa();
+        //ActionToolBar();
+   // }
+
+    //private void ActionToolBar() {
+       // try{
+       // }catch (Exception e ){
+
+       // }
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+   // }
+
+    //private void AnhXa() {
+        //toolbar = findViewById(R.id.toobar);
+        //recyclerView = findViewById(R.id.recycleview_dt);
+        //RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        //recyclerView.setLayoutManager(layoutManager);
+        //recyclerView.setHasFixedSize(true);
+    //}
+}
---
+++ app/src/main/java/com/example/appbanhang/activity/MainActivity.java
@@ -0,0 +1,197 @@
+package com.example.appbanhang.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+import com.example.appbanhang.R;
+import com.example.appbanhang.adapter.LoaiSpAdapter;
+import com.example.appbanhang.adapter.SanPhamMoiAdapter;
+import com.example.appbanhang.model.LoaiSp;
+import com.example.appbanhang.model.SanPhamMoi;
+import com.example.appbanhang.model.SanPhamMoiModel;
+import com.example.appbanhang.retrofit.ApiBanHang;
+import com.example.appbanhang.retrofit.RetrofitClient;
+import com.example.appbanhang.utils.Utils;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewManHinhChinh;
+    NavigationView navigationView;
+    ListView listViewManHinhChinh;
+    DrawerLayout drawerLayout;
+    LoaiSpAdapter loaiSpAdapter;
+    List<LoaiSp> mangloaisp;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+    ApiBanHang apiBanHang;
+    List<SanPhamMoi> mangSpMoi;
+    SanPhamMoiAdapter spAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+
+        Anhxa();
+        ActionBar();
+
+        if (isConnected(this)){
+
+            ActionViewFlipper();
+            getLoaiSanPham();
+            getSpMoi();
+            getEvenClick();
+        }else {
+            Toast.makeText(getApplicationContext(),"khong co internet, vui long ket noi", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void getEvenClick() {
+        listViewManHinhChinh.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        Intent trangchu = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(trangchu);
+                        break;
+                    case 1:
+                        Intent dienthoai = new Intent(getApplicationContext(), DienThoaiActivity.class);
+                        startActivity(dienthoai);
+                        break;
+                    case 2:
+                        Intent laptop = new Intent(getApplicationContext(), LaptopActivity.class);
+                        startActivity(laptop);
+                        break;
+                }
+            }
+        });
+    }
+
+
+    private void getSpMoi() {
+        compositeDisposable.add(apiBanHang.getSpMoi()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(
+                sanPhamMoiModel -> {
+                    if (sanPhamMoiModel.isSuccess()){
+                        mangSpMoi = sanPhamMoiModel.getResult();
+                        spAdapter = new SanPhamMoiAdapter(getApplicationContext(), mangSpMoi);
+                        recyclerViewManHinhChinh.setAdapter(spAdapter);
+
+                    }
+                },
+                throwable -> {
+                    Toast.makeText(getApplicationContext(), "Không kết nối được với sever"+throwable.getMessage(), Toast.LENGTH_LONG).show();
+                }
+        ));
+    }
+
+
+    private void getLoaiSanPham() {
+        compositeDisposable.add(apiBanHang.getLoaisp()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        loaiSpModel -> {
+                            if (loaiSpModel.isSuccess()){
+                                mangloaisp = loaiSpModel.getResult();
+                                loaiSpAdapter = new LoaiSpAdapter(getApplicationContext(),mangloaisp);
+                                listViewManHinhChinh.setAdapter(loaiSpAdapter);
+                            }
+                        }
+                ));
+
+    }
+
+    private void ActionViewFlipper() {
+        List<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://scontent.fdad1-2.fna.fbcdn.net/v/t39.30808-6/281764667_5399944363386021_2396060187041393882_n.png?_nc_cat=106&ccb=1-6&_nc_sid=e3f864&_nc_ohc=JsETAQYKbD8AX9IaWdz&_nc_ht=scontent.fdad1-2.fna&oh=00_AT_VxnObe9FCgPZ4GqCNaRngJrPTe2kTVJ4aIV8BbPti8w&oe=628A9F26");
+        mangquangcao.add("https://scontent.fdad1-2.fna.fbcdn.net/v/t39.30808-6/279949023_5364386160275175_7527768485195285192_n.jpg?_nc_cat=102&ccb=1-6&_nc_sid=e3f864&_nc_ohc=mW_EdR3f1RAAX85qTPU&_nc_oc=AQmjLp5Yz_u07Ow15-vUgUpMr6vdxg0_ArDMCE6QN9bTgvcBj35QmcCmbNf1OC0tJGY&_nc_ht=scontent.fdad1-2.fna&oh=00_AT-IJpDz0UzHcxBSq3sC8gGSA1oJdhXU4C4QYgxG77ugNQ&oe=628A2C8A");
+        mangquangcao.add("https://scontent.fdad2-1.fna.fbcdn.net/v/t39.30808-6/278794804_5348199645227160_2762865951263867922_n.png?_nc_cat=107&ccb=1-6&_nc_sid=e3f864&_nc_ohc=ikZk8RJ8IEkAX-yTBnL&_nc_ht=scontent.fdad2-1.fna&oh=00_AT8C7wwnMD2FMXFqO5L19jFuHdBjBe0-nutzjCVLu5CgxA&oe=628A1406");
+        for (int i = 0; i<mangquangcao.size(); i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setInAnimation(slide_out);
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void Anhxa() {
+        toolbar = findViewById(R.id.toobarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewlipper);
+        recyclerViewManHinhChinh = findViewById(R.id.recycleview);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerViewManHinhChinh.setLayoutManager(layoutManager);
+        recyclerViewManHinhChinh.setHasFixedSize(true);
+        listViewManHinhChinh = findViewById(R.id.listviewmanhinhchinh);
+        drawerLayout = findViewById(R.id.drawerlayout);
+        //khoi tao list
+        mangloaisp = new ArrayList<>();
+        mangSpMoi = new ArrayList<>();
+
+    }
+    private boolean isConnected (Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI); //nho them quyen vao khong bi loi
+        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if ((wifi != null && wifi.isConnected()) ||(mobile != null && mobile.isConnected()) ){
+            return true;
+        }else{
+            return false;
+        }
+        }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
+    }
+}
